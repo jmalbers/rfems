0e35ac7453c5bff20bd9cc06ceb42736f2af7cdd
Jacob M. Albers <jmalbers@gmail.com>
2025-01-09 13:06:50 -0700

    Refactored tests to use unittest module

--- ustrip/tests/stlimp_test1.py
+++ ustrip/tests/stlimp_test1.py
@@ -1,8 +1,8 @@
 import sys
 import os
 import logging
+import unittest
 
-#logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__file__)
 sys.path.append("../")
 from solverlib.stl import StlImporter
@@ -11,12 +11,22 @@ from solverlib.stl import StlImporter
 FILENAME = 'imp_test1.stl'
 STL_PATH = 'test_files/'
 
-logger.info('Starting STL Importer stl file read test.')
-imp = StlImporter()
-logger.info('Importer initialized!')
-imp.parse_stl(STL_PATH + FILENAME)
-logger.info('STL parsed.')
-tri = len(imp.imports[os.path.splitext(FILENAME)[0]])
-assert tri == 12, "XXXX STL Importer Test 1 FAIL: Expected 12 triangles."
-logger.info(f'SUCCESS: {tri} triangles parsed from {FILENAME}!')
-print("**** STL Importer Test 1: PASSED")
+class TestStlImporter(unittest.TestCase):
+
+    def setUp(self):
+        self.imp = StlImporter()
+        logger.info('Importer initialized!')
+
+    def test_triangleCount(self):
+        self.imp.parse_stl(STL_PATH + FILENAME)
+        logger.info('STL parsed.')
+        tri = len(self.imp.imports[os.path.splitext(FILENAME)[0]])
+        self.assertEqual(tri, 12, f'Expected 12 trianges got {tri}')
+        logger.info(f'SUCCESS: {tri} triangles parsed from {FILENAME}!')
+
+    def test_notZip(self):
+        with self.assertRaises(TypeError):
+            self.imp.unzip_models(STL_PATH + FILENAME)
+
+if __name__ == "__main__":
+    unittest.main()
