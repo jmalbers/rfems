80dae2ad1d1655284cfc3db365e6637217e77ad8
Jacob M. Albers <jmalbers@gmail.com>
2025-01-09 17:41:22 -0700

    Add stl name / param parser basic unit tests

--- ustrip/tests/stlnam_test1.py
+++ ustrip/tests/stlnam_test1.py
@@ -5,8 +5,9 @@ sys.path.append("../")
 from solverlib.stl import StlNameParser
 
 FILE_PATH = 'test_files/'
-GOOD_NAME = ['ustrip', 'mat', 'pri', 'num', '']
-BAD_NAME  = 'bad'
+GOOD_FILE_NAME = ['ustrip', 'mat=silver', 'pri=1', 'n=2']
+BAD_NAME  = ['horses', 'mat=gold', 'pri=1']
+BAD_ARGS  = ['ustrip', 'mat=silver', 'beef=1', 'n=2']
 
 class TestStlNameParserBasic(unittest.TestCase):
 
@@ -14,22 +15,39 @@ class TestStlNameParserBasic(unittest.TestCase):
         self.uut = StlNameParser()
 
     def test_split_name(self):
-        ...
+        t = ' '.join(GOOD_FILE_NAME)
+        fn = self.uut._split_name(t)
+        self.assertEqual(len(GOOD_FILE_NAME), len(fn))
+
+        t = '_'.join(GOOD_FILE_NAME)
+        fn = self.uut._split_name(t)
+        self.assertEqual(len(GOOD_FILE_NAME), len(fn))
 
     def test_return_args(self):
-        ...
+        r = self.uut._return_args(GOOD_FILE_NAME)
+        for i in range(1, len(GOOD_FILE_NAME)-1):
+            self.assertEqual(r[i-1], GOOD_FILE_NAME[i])
 
     def test_get_argval(self):
-        ...
+        a, v = self.uut._get_argval(GOOD_FILE_NAME[1])
+        self.assertEqual(a, 'mat')
+        self.assertEqual(v, 'silver')
 
     def test_parse_badname(self):
-        ...
+        r = self.uut.parse_filename(' '.join(BAD_NAME))
+        self.assertFalse(r)
+        self.assertEqual(len(self.uut.parsed), 0)
     
     def test_parse_goodname(self):
-        ...
+        r = self.uut.parse_filename(' '.join(GOOD_FILE_NAME))
+        self.assertTrue(r)
+        self.assertEqual(len(self.uut.parsed), len(GOOD_FILE_NAME))
 
-    def test_parse_port(self):
-        ...
+    def test_bad_args(self):
+        r = self.uut.parse_filename(' '.join(BAD_ARGS))
+        self.assertTrue(r)
+        self.assertFalse('beef' in self.uut.parsed.keys())
+        self.assertTrue('n' in self.uut.parsed.keys())
 
 if __name__ == "__main__":
     unittest.main()
